d449fab56c48c3488e9f36fc49fc3a4416e2e296
TemitopeOgunrekun <olalekanogunrekun@gmail.com>
2023-08-23 16:48:10 +0100

    styling the hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,43 @@
 import React from "react";
+import {
+	Navbar,
+	Hero,
+	Stats,
+	Business,
+	Billing,
+	CardDeal,
+	Testimonials,
+	Clients,
+	CTA,
+	Footer,
+} from "./component";
 import styles from "./styles";
 
 const App = () => (
 	<div className="bg-primary w-full overflow-hidden">
 		<div className={`${styles.paddingX} ${styles.flexCenter} `}>
-			<div className={`${styles.boxWidth}`}>Navbar</div>
+			<div className={`${styles.boxWidth}`}>
+				<Navbar />
+			</div>
 		</div>
 
 		<div className={`bg-primary ${styles.flexStart}`}>
-			<div className={`${styles.boxWidth}`}>Hero</div>
+			<div className={`${styles.boxWidth}`}>
+				<Hero />
+			</div>
 		</div>
 
 		<div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        stats
-        business
-        billing
-        cardDeal
-        testimonials
-        client
-        Cta
-        footer
-
-      </div>
+			<div className={`${styles.boxWidth}`}>
+				<Stats />
+				<Business />
+				<Billing />
+				<CardDeal />
+				<Testimonials />
+				<Clients />
+				<CTA />
+				<Footer />
+			</div>
 		</div>
 	</div>
 );
