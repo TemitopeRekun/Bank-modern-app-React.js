d92f98e0a88efdd0b2983eb7bde1cc7f2805d3be
TemitopeOgunrekun <olalekanogunrekun@gmail.com>
2023-08-23 22:44:57 +0100

    started styling the testimonial component

--- src/component/Business.jsx
+++ src/component/Business.jsx
@@ -3,6 +3,29 @@ import { features } from "../constant";
 import styles, { layout } from "../styles";
 import Button from "./Button";
 
+const FeatureCard = ({ icon, title, content, index }) => (
+	<div
+		className={`flex flex-row p-6 rounded-[20px] 
+      ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
+		<div
+			className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+			<img
+				src={icon}
+				alt="icons"
+				className="w-[50%] h-[50%] object-contain"
+			/>
+		</div>
+		<div className="flex flex-1 flex-col ml-3">
+			<h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">
+				{title}
+			</h4>
+			<p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1">
+				{content}
+			</p>
+		</div>
+	</div>
+);
+
 const Business = () => (
 	<section id="features" className={layout.section}>
 		<div className={layout.sectionInfo}>
@@ -20,7 +43,7 @@ const Business = () => (
 
 		<div className={`${layout.sectionImg} flex flex-col`}>
 			{features.map((feature, index) => (
-				<FeatureCard />
+				<FeatureCard key={feature.id} {...feature} index={index} />
 			))}
 		</div>
 	</section>
