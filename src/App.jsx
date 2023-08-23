1dd57e0d106fad9ae6438959394cb85873420e46
TemitopeOgunrekun <olalekanogunrekun@gmail.com>
2023-08-23 14:25:44 +0100

    added assets and created component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import styles from "./styles";
 
-const App = () => {
-	return <div>App</div>;
-};
+const App = () => (
+	<div className="bg-primary w-full overflow-hidden">
+		<div className={`${styles.paddingX} ${styles.flexCenter} `}>
+			<div className={`${styles.boxWidth}`}>Navbar</div>
+		</div>
+
+		<div className={`bg-primary ${styles.flexStart}`}>
+			<div className={`${styles.boxWidth}`}>Hero</div>
+		</div>
+
+		<div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        stats
+        business
+        billing
+        cardDeal
+        testimonials
+        client
+        Cta
+        footer
+
+      </div>
+		</div>
+	</div>
+);
 
 export default App;
