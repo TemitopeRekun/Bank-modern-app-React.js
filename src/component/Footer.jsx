fb58531d9a616a102b7156470dd06de34bac4127
TemitopeOgunrekun <olalekanogunrekun@gmail.com>
2023-08-24 16:41:18 +0100

    finished the project

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -16,6 +16,47 @@ const Footer = () => (
 					A new way to make the payments easy, reliable and secure.
 				</p>
 			</div>
+
+			<div className="flex-[1.5] flex flex-row justify-between flex-wrap w-full md:mt-0 mt-10">
+				{footerLinks.map((footerLink) => (
+					<div
+						key={footerLink.key}
+						className="flex flex-col ss:my-0 my-4 min-w-[150px]">
+						<h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white hover:text-secondary">
+							{footerLink.title}
+						</h4>
+						<ul className="mt-4 list-none">
+							{footerLink.links.map((link, index) => (
+								<li
+									key={link.name}
+									className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${
+										index !== footerLink.length - 1 ? "mb-4" : "mb-0"
+									}`}>
+									{link.name}
+								</li>
+							))}
+						</ul>
+					</div>
+				))}
+			</div>
+		</div>
+
+		<div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
+			<p className="font-poppins text-white font-normal text-center text-[18px] leading-[27px] ">
+				Copyright 2021 HooBank. All Rights Reserved.
+			</p>
+			<div className="flex flex-row md:mt-0 mt-6">
+				{socialMedia.map((social, index) => (
+					<img
+						key={social.id}
+						src={social.icon}
+						alt={social.id}
+						className={`w-[21px] h-[21px] object-contain cursor-pointer ${
+							index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
+						}`}
+					/>
+				))}
+			</div>
 		</div>
 	</section>
 );
